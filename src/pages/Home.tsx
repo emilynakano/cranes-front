e7343345ab7c54107319f99cda9c020462e92204
emily <nakanodepaula@gmail.com>
2022-12-20 20:16:44 -0300

    feat: add images in the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {  StyleSheet, Text, View, Image, ScrollView } from 'react-native'
+import Tf1 from '../../assets/Tf1.png'
+import Tf2 from '../../assets/Tf2.png'
 
 const Home = () => {
   return (
-    <View style={ { backgroundColor: 'red', flex: 1 } }>
-      <Text>{'home'.repeat(100)}</Text>
-    </View>
+    <ScrollView style={ {  flex: 1  } }>
+      <View style={{
+        position: 'relative', 
+        flexDirection: 'row',
+        alignItems:'flex-end', 
+        justifyContent:'space-between',
+        height: 460,
+        marginLeft: -50,
+        marginRight: -20
+        }}>
+        <Image style={{
+          position: 'absolute', 
+          zIndex: 1,
+          maxWidth: 300,
+          height:380,
+          }} source={Tf2} />
+        <View style={{       
+          width: '100%',
+          alignItems: 'flex-end'
+          } 
+        }>
+        <Image source={Tf1} style={{       
+          maxWidth: 230,
+          height:290,
+          } 
+        }/>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
 
