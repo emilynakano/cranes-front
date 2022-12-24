a51f81d793125632e631092059bd91e57dd8b47f
emily <nakanodepaula@gmail.com>
2022-12-23 22:16:26 -0300

    feat: input with material UI

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,9 @@
+import { TextInput } from '@react-native-material/core';
+
+function Input() {
+  return (
+    <TextInput variant="standard" label="Email Adresses" style={{ margin: 50 }} />
+  );
+}
+
+export default Input;
