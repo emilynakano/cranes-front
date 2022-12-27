38112008bbf580e46536a626101eeec6f0f4279e
emily <nakanodepaula@gmail.com>
2022-12-27 15:34:15 -0300

    feat: adjusts and use of map function in the auth handler

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import { Container, DownSide, UpSide } from './styles';
+import { Text } from '@react-native-material/core';
+import React, { useState } from 'react';
+import { FlatList } from 'react-native';
+import {
+  AuthOption,
+  AuthOptions, Container, DownSide, Row, UpSide,
+} from './styles';
 
 export default function Auth() {
+  const authOptions: [string, string] = ['Login', 'Sign-up'];
+  const [authSelected, setAuthSelected] = useState('Sign-up');
   return (
     <Container>
-      <UpSide />
+      <UpSide>
+        <Text>
+          logo
+        </Text>
+        <AuthOptions>
+          {authOptions.map((_auth) => (
+            <AuthOption onPress={() => setAuthSelected(_auth)} activeOpacity={1}>
+              <Text>
+                {_auth}
+              </Text>
+              <Row authSelected={authSelected} authActual={_auth} />
+            </AuthOption>
+          ))}
+        </AuthOptions>
+      </UpSide>
       <DownSide />
     </Container>
   );
--- src/pages/Auth/styles.ts
+++ src/pages/Auth/styles.ts
@@ -9,8 +9,34 @@ export const UpSide = styled.View`
   flex: 0.4;
   background: ${({ theme }) => theme.COLORS.background_light};
   border-radius: 30px;
+
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const DownSide = styled.View`
   flex: 0.6;
+`;
+
+export const AuthOptions = styled.View`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 50px;
+  gap: 10px;
+`;
+
+export const AuthOption = styled.TouchableOpacity`
+  width: 50%;
+  height: 36px;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Row = styled.View`
+  width: 100%;
+  height: 3px;
+  background-color: ${(props) => (props.authSelected === props.authActual ? props.theme.COLORS.details : 'transparent')};
+  transition: 0.3s ease-out;
 `;
