8358fc13f8ad47bd3641c119b57ddd6639c5061b
emily <nakanodepaula@gmail.com>
2023-01-04 10:07:18 -0300

    feat: signUp layout

--- src/pages/Auth/SignUp.tsx
+++ src/pages/Auth/SignUp.tsx
@@ -1,11 +1,35 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import { TextInput } from 'react-native-paper';
+import {
+  Container, Main, Input, Button,
+} from './styleForm';
 
 function SignUp() {
+  const [checkPassword, setCheckPassword] = useState(false);
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <Container>
+      <Main>
+        <Input
+          mode="outlined"
+          label="Name"
+        />
+        <Input
+          mode="outlined"
+          label="Email"
+        />
+        <Input
+          mode="outlined"
+          label="Password"
+          secureTextEntry={!checkPassword}
+          right={checkPassword
+            ? <TextInput.Icon icon="eye-off" onPress={() => setCheckPassword(!checkPassword)} />
+            : <TextInput.Icon icon="eye" onPress={() => setCheckPassword(!checkPassword)} />}
+        />
+      </Main>
+      <Button mode="contained" onPress={() => console.log('Pressed')}>
+        Sign-up
+      </Button>
+    </Container>
   );
 }
 
